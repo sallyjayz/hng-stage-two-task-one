c5efc5209c76f46719fcf9c967588161c98bd90b
sallyjayz <salama.jatau@yahoo.com>
2021-08-19 17:01:09 +0100

    completed task

---
+++ PrintName.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class PrintName  
+{   
+    public static void main(String[] args)   
+    {   
+        //declaration of name   
+        String myName = "Salama Nyela Jatau";
+
+        //prints the values on the console  
+        System.out.println(); 
+        System.out.print(myName); 
+        System.out.println();
+    }   
+}  
